f2c8030750142c79a82e897e5a57804b1cdffe0e
Valery Kovalev <kovalev-vxx@MacBook-Pro-Valery.local>
2023-02-07 21:45:31 +0300

    docket fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express, {Express, Request, Response} from 'express';
 import expressWs from "express-ws";
 
+const cors = require("cors")
 const {app, getWss, applyTo} = expressWs(express())
 const port = process.env.PORT ?? 8000;
+
+app.use(cors)
+app.use(express.json())
 
 app.ws("/", (ws, req) => {
     console.log("Connected")
